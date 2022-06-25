985c054edd728a17e2bf9ca5002e8165db3e20eb
Quentin Bernet <quentin.bernet@bluewin.ch>
2022-06-25 19:50:43 +0200

    Cleanup

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -30,7 +30,7 @@ def printSet(s: String): Unit =
 
   //val ctx: Setr = "<5 1 3>"//"<0 <0 4> 2 <3> <2 3>>"
   val show  = "dn"   // prints the context
-  val clear = "([r]" // empties context
+  val clear = "([r]" //) empties context
   val unionWithWrapped = "ua"
   inline def ifmap(cond: String)(f: String) = s"($cond{$f}" //)
   inline def map(f: String) = ifmap("<>")(f) // ctx.map(f) where f is a function
@@ -40,12 +40,11 @@ def printSet(s: String): Unit =
   val negate = "u-1" // if ctx == 0 then 1 else if ctx == 1 then 0
   val isNonEmpty = map(clear) // booleifies ctx: 0 => 0   everything else => 1
   val isEmpty = isNonEmpty ++ negate
-  val contains0 = s"({$clear 0} -<<>>"
+  val contains0 = s"({$clear 0} -<<>>" //)
   inline def notEquals(const: String) = s"u -$const $isNonEmpty"
   inline def equals(const: String) = notEquals(const) ++ negate
   inline def exists(cond: String) = filter(cond) ++ isNonEmpty
-  inline def contains(const: String) = exists(equals(const)) //) if ctx contains const then 1 else 0
-  
+  inline def contains(const: String) = exists(equals(const)) // if ctx contains const then 1 else 0
   eval("uu0ua-0", "(2 3)")
   /*
   inline def elvis(p: String, q: String) = // if ctx then p else q assuming ctx == 0 or ctx == 1
